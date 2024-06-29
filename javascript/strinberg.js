b32baf5ae3b88214cb857d586a589b76ea3ece53
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-06-29 17:15:56 -0300

    MSC Store

---
+++ javascript/strinberg.js
@@ -0,0 +1,49 @@
+document.getElementById("bt+product").addEventListener("click", increment_product)
+document.getElementById("bt-product").addEventListener("click", decrement_product)
+
+//pointer
+document.getElementById("bt+product").addEventListener("mouseover", pointer)
+document.getElementById("bt-product").addEventListener("mouseover", pointer)
+
+function pointer(){
+    this.style.cursor = "pointer"
+}
+
+function increment_product(){
+    let amount = parseInt(document.getElementById("amount_product").value)
+    if (amount < 15){
+        amount++
+        document.getElementById("amount_product").value = amount
+    } else {
+        alert("Entre em contato para compar quantidades maiores.")
+    }
+    total()
+}
+
+function decrement_product(){
+    let amount = parseInt(document.getElementById("amount_product").value)
+    if (amount > 0){
+        amount--
+        document.getElementById("amount_product").value = amount
+    }
+    total()
+}
+
+
+function total(){
+    const price_guitar_v = 3999.90
+    let amount = parseInt(document.getElementById("amount_product").value)
+    let total = parseFloat(price_guitar_v * amount).toFixed(2)
+    document.getElementById("total").value = "R$ " + total.replace('.',',')
+}
+
+
+function complete_purchase(){
+    if((parseInt(document.getElementById("amount_product").value) > 0)){
+        alert("Compra realizada com sucesso!\nObrigado pela compra")
+        document.getElementById("amount_product").value = 0
+        document.getElementById("total").value = "R$ 0,00"
+    }
+}
+
+document.getElementById("buy_now").addEventListener("click", complete_purchase)
